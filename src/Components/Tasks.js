8d1f932ad73f27b986d2aad960019cbc2baadf37
Scott Kuchinski <skuch@ugao.upenn.edu>
2017-02-14 14:09:59 -0500

    First Commit, App Scaffold

---
+++ src/Components/Tasks.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import {Panel} from 'muicss/react';
+
+
+class Tasks extends Component {
+  render() {
+    return (
+    <Panel>
+      TASKS
+    </Panel>
+    );
+  }
+}
+
+export default Tasks;
